ed334a5ce545269b447cf25ea317ea9a2ea435a9
Kaua-S17 <00001116871312sp@al.educacao.sp.gov.br>
2024-11-13 11:35:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'o que é',
+    'O que é engenharia?',
+    'Conjunto de técnicas e métodos para aplicar o conhecimento técnico e científico na planificação, criação e manutenção de estruturas, máquinas e sistemas para benefício do ser humano'
+)
+
+criaCartao(
+    'qual á',
+    'Qual a maior fabricante de carros no brasil?',
+    'Volkswagen'
+)
+
+criaCartao(
+    'por que',
+    'por que a sociologia é importante?',
+    'A Sociologia nos auxilia a fazermos reflexões como sujeitos pertencentes a um mundo social o qual nos constrói socialmente e ao mesmo tempo é construído por nós.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Obrigado em Inglês?',
+    'Obrigado em ingles é Thanks'
+)
